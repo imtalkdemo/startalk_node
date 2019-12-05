a62e837d1e7f9ba69483245aa84d259d652ad22d
zhangchao11 <zhangchao11@360.cn>
2019-12-05 12:09:46 +0800

    modify config

--- node_config.js
+++ node_config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    "fileurl": "http://dev.fsll.tech:8080",
-    "javaurl": "http://dev.fsll.tech:8080/package",
-    "httpurl": "http://dev.fsll.tech:8080/newapi",
-    "apiurl": "http://dev.fsll.tech:8080/api",
-    "xmpp": "dev.fsll.tech:5280",
+    "fileurl": "http://qtalk:8080",
+    "javaurl": "http://qtalk:8080/package",
+    "httpurl": "http://qtalk:8080/newapi",
+    "apiurl": "http://qtalk:8080/api",
+    "xmpp": "qtalk:5280",
     "web":{
         "title":'Qchat Web',
         "webcss": '/styles/web/page/' + 'qchat@24ee0d5a61d98bceb450b5412f544247.css',
-        "webjs": '/scripts/page/web/' + 'qchat@c19563ed59b91f8b455f70151e579de4.js'
+        "webjs": '/scripts/page/web/' + 'qchat@bce4c53679ed132671c7b01d10423bfc.js'
     },
     "touch":{
         "title":'Qchat Touch',
         "touchcss": '/styles/touch/page/' + 'qchat@268bda38e7c91e086867bf7f0da484fd.css',
-        "touchjs": '/scripts/page/touch/' + 'qchat@89c3c8ab8028dde9e9ca1d4539422f28.js'
+        "touchjs": '/scripts/page/touch/' + 'qchat@c6a6b8efdf8c28b3b1fa810bc3485fec.js'
     },
     "jquery": '/scripts/lib/jquery/'+ 'jquery_1.7@88c8777bbcb1fb174b5edf2c1dd03278.js',
-    "navigation": '/' + "navigation@aa2c9d0505f8bc7b50b3d659a544f24f.js"
+    "navigation": '/' + "navigation@4ea1d24259a042e3258f9b637fdfa521.js"
 }
